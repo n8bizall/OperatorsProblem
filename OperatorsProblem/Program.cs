6d8bc5d784da5d0f47587f143db67fad242ac665
Nate Bucher <n8bizall@yahoo.com>
2018-02-21 09:34:57 -0500

    commit solution

--- OperatorsProblem/Program.cs
+++ OperatorsProblem/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace OperatorsProblem
+class Solution
 {
-    class Program
+
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        double meal_cost = Convert.ToDouble(Console.ReadLine());
+        int tip_percent = Convert.ToInt32(Console.ReadLine());
+        int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+
+        double tip = ((Convert.ToDouble(tip_percent) / 100) * meal_cost);
+        double tax = ((Convert.ToDouble(tax_percent) / 100) * meal_cost);
+        double totalCost = (meal_cost + tip + tax);
+
+        int roundedTotal = Convert.ToInt32(Math.Round(totalCost, 0));
+
+        Console.WriteLine($"The total meal cost is {roundedTotal} dollars.");
+        Console.ReadKey();
+
     }
+
 }
